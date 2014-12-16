287cfdefac7bfc2391fd88d8cbf8a84d58ff07ae
a1260766 <a1260766@Einherjar>
2014-12-16 12:12:57 -0200

    ultimo commit

---
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/PoligonalFechada.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utfpr.ct.dainf.if62c.avaliacao;
+
+/**
+ *
+ * @author a1260766
+ */
+public class PoligonalFechada extends Poligonal{
+
+    public PoligonalFechada(int n) {
+        super(n);
+    }
+    
+    @Override
+    public double getComprimento(){
+        double comprimento=0;
+        
+        return comprimento;
+    }
+    
+}
